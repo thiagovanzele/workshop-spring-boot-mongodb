c6cc5a2aa10809c514079948a169ccaa516d905e
Thiago Vanzele <thiagovanzele@gmail.com>
2023-01-12 16:17:12 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/thiagovanzele/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagovanzele.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
