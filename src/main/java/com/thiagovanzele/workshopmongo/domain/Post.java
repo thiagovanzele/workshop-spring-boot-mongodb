42ea4d6eee44f2d2a3e4b5b926f23f4813a75ee0
Thiago Vanzele <thiagovanzele@gmail.com>
2023-01-20 17:10:11 -0300

    Acrescentando comentario aos posts

--- src/main/java/com/thiagovanzele/workshopmongo/domain/Post.java
+++ src/main/java/com/thiagovanzele/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.thiagovanzele.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.thiagovanzele.workshopmongo.dto.AuthorDTO;
+import com.thiagovanzele.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -30,6 +33,8 @@ public class Post implements Serializable {
 		this.body = body;
 		this.author = author;
 	}
+
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public String getId() {
 		return id;
@@ -62,13 +67,21 @@ public class Post implements Serializable {
 	public void setBody(String body) {
 		this.body = body;
 	}
-	
+
 	public AuthorDTO getAuthor() {
 		return author;
 	}
-	
+
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
@@ -87,6 +100,5 @@ public class Post implements Serializable {
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
-
 
 }
